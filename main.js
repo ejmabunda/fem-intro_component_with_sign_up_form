4cd74ceb439ec92c8f053213c828cfb5350583ab
Junior <mjmabunda0@gmail.com>
2024-01-21 10:42:31 +0200

    Implement simple Constraint Validation API validation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const form = document.getElementById("form");
 
 const fname = document.getElementById("fname");
 const fname_error = document.getElementById("fname_error");
+const fname_error_img = document.getElementById("fname_error_img");
 
 const lname = document.getElementById("lname");
 const lname_error = document.getElementById("lname_error");
@@ -16,8 +17,42 @@ fname.addEventListener("input", (event) => {
   if (fname.validity.valid) {
     fname_error.textContent = "";
     fname_error.className = "error";
+    fname_error_img.style.display = "none";
   } else {
     showFNameError();
+    event.preventDefault();
+  }
+});
+
+lname.addEventListener("input", (event) => {
+  if (lname.validity.valid) {
+    lname_error.textContent = "";
+    lname_error.className = "error";
+    lname_error_img.style.display = "none";
+  } else {
+    showLNameError();
+    event.preventDefault();
+  }
+});
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    email_error.textContent = "";
+    email_error.className = "error";
+    email_error_img.style.display = "none";
+  } else {
+    showEmailError();
+    event.preventDefault();
+  }
+});
+
+password.addEventListener("input", (event) => {
+  if (password.validity.valid) {
+    password_error.textContent = "";
+    password_error.className = "error";
+    password_error_img.style.display = "none";
+  } else {
+    showPasswordError();
     event.preventDefault();
   }
 });
@@ -27,6 +62,21 @@ form.addEventListener("submit", (event) => {
     showFNameError();
     event.preventDefault();
   }
+
+  if (!lname.validity.valid) {
+    showLNameError();
+    event.preventDefault();
+  }
+
+  if (!email.validity.valid) {
+    showEmailError();
+    event.preventDefault();
+  }
+
+  if (!password.validity.valid) {
+    showPasswordError();
+    event.preventDefault();
+  }
 });
 
 function showFNameError() {
@@ -34,4 +84,29 @@ function showFNameError() {
     fname_error.textContent = "First Name cannot be empty";
 
   fname_error.className = "error.active";
+  fname_error_img.style.display = "inline-block";
+}
+
+function showLNameError() {
+  if (lname.validity.valueMissing)
+    lname_error.textContent = "Last Name cannot be empty";
+
+  lname_error.className = "error.active";
+  lname_error_img.style.display = "inline-block";
+}
+
+function showEmailError() {
+  if (email.validity.valueMissing)
+    email_error.textContent = "Looks like this is not an email";
+
+  email_error.className = "error.active";
+  email_error_img.style.display = "inline-block";
+}
+
+function showPasswordError() {
+  if (password.validity.valueMissing)
+    password_error.textContent = "Password cannot be empty";
+
+  password_error.className = "error.active";
+  password_error_img.style.display = "inline-block";
 }
